7574a10d0ad5080056212da76a3c19801afb2448
andreascalzo99 <a.scalzo3@studenti.uniba.it>
2026-07-16 17:19:13 +0200

    aggiunto repository prenotazione

---
+++ src/main/java/com/accenture/WebAppVoli/repository/PrenotazioneRepository.java
@@ -0,0 +1,9 @@
+package com.accenture.WebAppVoli.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.accenture.WebAppVoli.model.Prenotazione;
+
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Integer> {
+
+}
