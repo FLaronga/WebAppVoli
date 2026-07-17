8f47eb6d1936a540f9553602a677aaaedfe5fee0
Federica Margherita Laronga <federicalaronga@gmail.com>
2026-07-17 14:46:28 +0200

    modifiche front-end

--- src/main/resources/Front_end/Javascript/animazioni.js
+++ src/main/resources/Front_end/Javascript/animazioni.js
@@ -7,27 +7,14 @@ fetch('iconaLogin.html')
     })
     .catch(error => console.error('Errore nel caricamento dell\'icona:', error));
 
-
-/*pop-up apertura */
-/*document.addEventListener("DOMContentLoaded", function() {
-  const schermataOscura = document.getElementById("sfondoPopup");
-  const pulsanteChiudi = document.getElementById("pulsanteChiudiPopup");
-
-  // 1. Mostra il pop-up all'apertura della pagina
-  schermataOscura.style.display = "block";
-
-  // 2. Chiudi il pop-up quando si clicca sulla X
-  pulsanteChiudi.addEventListener("click", function() {
-    schermataOscura.style.display = "none";
-  });
-
-  // 3. Chiudi il pop-up se si clicca fuori dalla finestra bianca (sullo sfondo scuro)
-  schermataOscura.addEventListener("click", function(evento) {
-    if (evento.target === schermataOscura) {
-      schermataOscura.style.display = "none";
-    }
-  });
-});*/
+/*chiusura pop-up*/
+function chiudiFinestra() {
+    document.getElementById('finestra-popup').style.display = 'none';
+}
 
 /*login-registrazione*/
+
+function accesso(){
+  window.location.href="../html/Reg_Aut.html"
+}
 
